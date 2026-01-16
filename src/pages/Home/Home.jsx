ddcb81a495f0733a3273787d52c8b93603a03766
Artem <suleymanov2007artem@gmail.com>
2026-01-16 17:05:29 +0200

    add hero video background

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,15 @@ const Home = () => {
 
   return (
     <section className={s.hero}>
+      <video className={s.hero_video}
+    autoPlay
+    muted
+    loop
+    playsInline
+>
+  <source src="/public/video/car_video.mp4" type="video/mp4"/>
+</video>
+<div className={s.hero_overlay}></div>
       <div className={s.hero_content}>
         <h1 className={s.hero_title}>Find your perfect rental car</h1>
         <p className={s.hero_txt}>
